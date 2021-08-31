309ae6902b57cb0402473c2898adad9daf8c058a
KotoAKira <68285452+KotoAKira@users.noreply.github.com>
2021-08-31 17:25:29 +0300

    Revert "helpers refactored"

--- src/pages/Board/components/Column/Column.tsx
+++ src/pages/Board/components/Column/Column.tsx
@@ -2,140 +2,102 @@ import React, { ReactElement } from "react";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import AddIcon from "@material-ui/icons/Add";
-import { useDispatch } from "react-redux";
+import { deleteColumnHandler } from "../../helpers/DeleteHandlers";
+import {
+  dragColumnOverHandler,
+  dragColumnStartHandler,
+  dragItemStartHandler,
+  dropColumnHandler,
+  dropItemHandler,
+} from "../../helpers/DragHandlers";
 import Item from "../Item/Item";
 import ColumnProps from "./types";
 import useStyles from "./styles";
-import { asyncUpdateBoardAction } from "../../../../store/actions/boards";
-import dropColumn from "../../helpers/drag/DropColumn";
-import dropItemInColumn from "../../helpers/drag/DropItemInColumn";
-import deleteColumn from "../../helpers/delete/DeleteColumn";
-import addItem from "../../helpers/dialog/AddItem";
-import { showModal } from "../../../../store/actions/modal";
-import { addItemContent, editColumnNameContent } from "../../consts/consts";
-import editColumnName from "../../helpers/dialog/EditColumnName";
 
 const Column = ({
   column,
+  editItemClickHandler,
+  columnIndex,
   board,
   boardId,
+  dispatch,
+  editColumnNameClickHandler,
+  addItemClickHandler,
   currentDragColumnOfItem,
   currentDragItem,
   currentDragColumn,
+  dragType,
   setCurrentDragColumnOfItem,
   setCurrentDragItem,
+  setDragType,
   setCurrentDragColumn,
 }: ColumnProps): ReactElement => {
   const classes = useStyles();
-  const dispatch = useDispatch();
-
-  const handleColumnDragOver = (e: React.DragEvent<HTMLElement>) => {
-    e.preventDefault();
-  };
-
-  const handleColumnDragStart = (e: React.DragEvent<HTMLElement>) => {
-    e.dataTransfer.setData("text/plain", "column");
-    setCurrentDragColumn(column);
-  };
-
-  const handleColumnDrop = (e: React.DragEvent<HTMLElement>) => {
-    if (e.dataTransfer.getData("text") === "column") {
-      dispatch(
-        asyncUpdateBoardAction({
-          board: dropColumn(column.id, currentDragColumn.id, board),
-          boardId,
-        })
-      );
-    } else {
-      dispatch(
-        asyncUpdateBoardAction({
-          board: dropItemInColumn(
-            currentDragColumnOfItem.id,
-            column.id,
-            currentDragItem,
-            board
-          ),
-          boardId,
-        })
-      );
-    }
-  };
-
-  const handleDeleteColumn = () => {
-    dispatch(
-      asyncUpdateBoardAction({
-        board: deleteColumn(column.id, board),
-        boardId,
-      })
-    );
-  };
-
-  const addItemHandler = (columnId: string) => (text: string) => {
-    dispatch(
-      asyncUpdateBoardAction({
-        board: addItem(board, columnId, text),
-        boardId,
-      })
-    );
-  };
-  const addItemClickHandler = () => {
-    dispatch(
-      showModal({
-        mainContent: addItemContent,
-        handler: addItemHandler(column.id),
-      })
-    );
-  };
-
-  const editColumnNameHandler = (columnTitle: string) => {
-    dispatch(
-      asyncUpdateBoardAction({
-        board: editColumnName(board, column.id, columnTitle),
-        boardId,
-      })
-    );
-  };
-  const editColumnNameClickHandler = () => {
-    dispatch(
-      showModal({
-        mainContent: editColumnNameContent,
-        handler: editColumnNameHandler,
-      })
-    );
-  };
-
   return (
     <div
       draggable
-      onDragStart={handleColumnDragStart}
-      onDrop={handleColumnDrop}
-      onDragOver={handleColumnDragOver}
+      onDragStart={dragColumnStartHandler(
+        column,
+        setCurrentDragColumn,
+        setDragType
+      )}
+      onDrop={dropColumnHandler(
+        column,
+        currentDragColumn,
+        board,
+        dragType,
+        currentDragItem,
+        currentDragColumnOfItem,
+        boardId,
+        dispatch
+      )}
+      onDragOver={dragColumnOverHandler}
       className={classes.column}
       key={column.id}
     >
       <div className={classes.columnTitle}>{column.columnTitle}</div>
       <EditOutlinedIcon
         className={classes.editColumnIcon}
-        onClick={editColumnNameClickHandler}
+        onClick={editColumnNameClickHandler(columnIndex, column)}
       />
       <DeleteOutlineIcon
         className={classes.deleteColumnIcon}
-        onClick={handleDeleteColumn}
+        onClick={deleteColumnHandler(column, board, boardId, dispatch)}
       />
-      <AddIcon className={classes.addIcon} onClick={addItemClickHandler} />
+      <AddIcon
+        className={classes.addIcon}
+        onClick={addItemClickHandler(columnIndex, column)}
+      />
       <div className={classes.columnContent}>
         {column.items &&
-          column.items.map((item) => (
+          column.items.map((item, itemIndex) => (
             <Item
               key={item.id}
               column={column}
+              columnIndex={columnIndex}
               item={item}
+              itemIndex={itemIndex}
               board={board}
+              dispatch={dispatch}
               boardId={boardId}
-              currentDragColumnOfItem={currentDragColumnOfItem}
-              currentDragItem={currentDragItem}
-              setCurrentDragColumnOfItem={setCurrentDragColumnOfItem}
-              setCurrentDragItem={setCurrentDragItem}
+              editItemClickHandler={editItemClickHandler}
+              dragItemStartHandler={dragItemStartHandler(
+                column,
+                item,
+                setCurrentDragColumnOfItem,
+                setCurrentDragItem,
+                setDragType
+              )}
+              dropItemHandler={dropItemHandler(
+                column,
+                item,
+                currentDragColumnOfItem,
+                currentDragItem,
+                board,
+                dragType,
+                boardId,
+                dispatch
+              )}
             />
           ))}
       </div>
--- src/pages/Board/components/Item/types.ts
+++ src/pages/Board/components/Item/types.ts
@@ -1,4 +1,6 @@
+import { Dispatch } from "redux";
 import React from "react";
+import { Action } from "redux-actions";
 import ColumnI from "../../../../interfaces/Column";
 import ItemI from "../../../../interfaces/Item";
 import BoardI from "../../../../interfaces/Board";
@@ -6,12 +8,19 @@ import BoardI from "../../../../interfaces/Board";
 interface ItemProps {
   column: ColumnI;
   item: ItemI;
+  dragItemStartHandler: () => (event: React.DragEvent<HTMLDivElement>) => void;
+  dropItemHandler: () => (event: React.DragEvent<HTMLDivElement>) => void;
+  editItemClickHandler: (
+    columnIndex: number,
+    column: ColumnI,
+    itemIndex: number,
+    item: ItemI
+  ) => () => void;
+  columnIndex: number;
+  itemIndex: number;
   board: BoardI;
   boardId: string;
-  currentDragItem: any;
-  currentDragColumnOfItem: any;
-  setCurrentDragColumnOfItem: React.Dispatch<ColumnI>;
-  setCurrentDragItem: React.Dispatch<ItemI>;
+  dispatch: Dispatch<Action<{ board: BoardI; boardId: string }>>;
 }
 
 export default ItemProps;
